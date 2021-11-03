0990ed4342a61e4dcea2b7911a78c2e02aaa6b41
mandic1908996 <marko.mandic.engr@gmail.com>
2021-11-03 21:55:24 +0100

    fixing storage

--- common/config/params.php
+++ common/config/params.php
@@ -6,4 +6,12 @@ return [
     'senderName' => 'Example.com mailer',
     'user.passwordResetTokenExpire' => 3600,
     'user.passwordMinLength' => 8,
+
+    'resourceManager' => [
+        's3.path.prefix' => 'local',
+        's3.file.prefix' => '',
+        'image.thumb.path' => 'thumbs/', //relative from path prefix
+        's3.expire.time' => '+20 minutes',
+        'image.thumb.expire' => '+20 minutes'
+    ],
 ];
