da44c6df4207b10e6172fe11ab3646de300bf3b9
mandic1908996 <marko.mandic.engr@gmail.com>
2021-11-22 00:16:58 +0100

    design fix

--- common/widgets/grid/views/_grid_layout.php
+++ common/widgets/grid/views/_grid_layout.php
@@ -9,7 +9,7 @@ use yii\web\View;
 ?>
 
 <div class="grid-layout">
-    <div class="row mb-4 flex-column-reverse flex-md-row">
+    <div class="row mb-2 flex-column-reverse flex-md-row">
         <div class="col-md-6">{search}</div>
         <div class="col-md-6 m-auto">
             <div class="d-flex align-items-center justify-content-md-end flex-wrap flex-md-nowrap">
